5960906e501e5c7fc5fe1c1a5c316924b13b7bfe
tadams95 <tyrelleadams28@gmail.com>
2023-01-09 15:32:49 -0800

    Add profile when logged in, redirect

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,9 +1,11 @@
 import { useState, useRef, useContext } from "react";
 import AuthContext from "../../store/auth-context";
+import { useHistory } from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
+  const history = useHistory();
   const emailInputRef = useRef();
   const passwordInputRef = useRef();
 
@@ -11,6 +13,8 @@ const AuthForm = () => {
 
   const [isLogin, setIsLogin] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
+
+
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
@@ -63,6 +67,7 @@ const AuthForm = () => {
       })
       .then((data) => {
         authCtx.login(data.idToken);
+        history.replace("/");
       })
       .catch((err) => {
         alert(err.message);
